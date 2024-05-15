7aacc8a9e32288b43321afb988c01b8727d1f353
Yutae11 <yutaokkotsu1110@gmail.com>
2024-05-16 02:13:26 +0530

    config.js

--- src/config.js
+++ src/config.js
@@ -2,22 +2,22 @@ require("dotenv").config();
 
 module.exports = {
   token: process.env.TOKEN || "", // your bot token
-  prefix: process.env.PREFIX || "*", // bot prefix
-  ownerID: process.env.OWNERID || ["823101214564417536"], //your discord id
+  prefix: process.env.PREFIX || "~", // bot prefix
+  ownerID: process.env.OWNERID || ["1219624638628233247"], //your discord id
   mongourl:
     process.env.MONGO_URI ||
-    "mongodb+srv://Rakibxox:omit968965@cluster0.lbvtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // MongoDb URL
+    "mongodb+srv://az782l:u9tivl@cluster0.mmfongd.mongodb.net/?retryWrites=true&w=majority", // MongoDb URL
   topggapi: "",
   embedColor: process.env.COlOR || "#303037", // embed colour
-  logs: process.env.LOGS || "1205476519158489138", // channel id for guild create and delete logs
+  logs: process.env.LOGS || "1218516170668249089", // channel id for guild create and delete logs
   langs: process.env.LANGS || "en",
   nodes: [
     {
-      host: process.env.NODE_HOST || "lava.link",
+      host: process.env.NODE_HOST || "lavalink.oryzen.xyz",
       identifer: process.env.NODE_ID || "Main",
 
       port: parseInt(process.env.NODE_PORT || "80"),
-      password: process.env.NODE_PASSWORD || "LAVA",
+      password: process.env.NODE_PASSWORD || "oryzen.xyz",
       secure: parseBoolean(process.env.NODE_SECURE || "false"),
     },
   ],
