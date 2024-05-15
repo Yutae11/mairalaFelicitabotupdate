63a62704371aad469243fc185cdac8006cdcefc2
Yutae11 <yutaokkotsu1110@gmail.com>
2024-05-16 03:00:11 +0530

    db.js

--- src/core/db.js
+++ src/core/db.js
@@ -1,6 +1,6 @@
 const { Database } = require('quickmongo');
 
-const db = new Database('mongodb+srv://pee33:wnn99e@cluster0.fgivnid.mongodb.net/?retryWrites=true&w=majority');
+const db = new Database('mongodb+srv://az782l:u9tivl@cluster0.mmfongd.mongodb.net/?retryWrites=true&w=majority');
 db.connect().then(() => console.log('[ MONGO DB ] Connected to Mongo Database!'));
 
 module.exports = db;
